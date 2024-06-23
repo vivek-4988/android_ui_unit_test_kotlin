6632bb453f0ff1559937fa9c50fc7a8dc6f1c00e
Vivek Shah <vivek-4988@users.noreply.github.com>
2024-06-23 08:52:00 +0530

    updated code

--- app/src/main/java/com/kotlin/unittest/MainActivity.kt
+++ app/src/main/java/com/kotlin/unittest/MainActivity.kt
@@ -1,19 +1,9 @@
 package com.kotlin.unittest
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kotlin.unittest.ui.theme.MyApplicationTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
